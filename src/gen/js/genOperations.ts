7fb2ead2c1df4b82408405ad5fbe91d60985cea3
jam13 <jamie.neil@gmail.com>
2018-08-20 17:10:18 +0100

    Revert export as fixed upstream

--- src/gen/js/genOperations.ts
+++ src/gen/js/genOperations.ts
@@ -155,7 +155,7 @@ function renderReturnSignature(op: ApiOperation, options: ClientOptions): string
   return `: Promise<api.Response<${getTSParamType(response)}>>`
 }
 
-export function getParamSignature(param: ApiOperationParam, options: ClientOptions): string[] {
+function getParamSignature(param: ApiOperationParam, options: ClientOptions): string[] {
   const signature = [getParamName(param.name)]
   if (options.language === 'ts') signature.push(getTSParamType(param))
   return signature
